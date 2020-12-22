e11e6e29411d48f584a9d62106cab443ed8584c8
Tom Brus <tombrus@gmail.com>
2020-12-22 20:55:36 +0100

    experiment

--- app/src/main/java/demo/app/App.java
+++ app/src/main/java/demo/app/App.java
@@ -12,7 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         Library library = new Library();
-        if (library.someLibraryMethod() && library.experiment()) {
+        if (library.someLibraryMethod() && library.experiment2()) {
             System.out.println(new App().getGreeting());
         }
     }
