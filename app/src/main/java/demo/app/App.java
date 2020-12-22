f88194b24da2e439c5ee0caf5c3b4776c3e6cf48
Tom Brus <tombrus@gmail.com>
2020-12-22 21:52:07 +0100

    calc branch

--- app/src/main/java/demo/app/App.java
+++ app/src/main/java/demo/app/App.java
@@ -12,7 +12,7 @@ public class App {
 
     public static void main(String[] args) {
         Library library = new Library();
-        if (library.someLibraryMethod() && library.experiment2()) {
+        if (library.someLibraryMethod() && library.exper()) {
             System.out.println(new App().getGreeting());
         }
     }
