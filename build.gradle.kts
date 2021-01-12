b797ab1facb95f0caa250667b6a9742a42c6d7ad
Tom Brus <tombrus@gmail.com>
2021-01-12 14:44:04 +0100

    sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -13,10 +13,8 @@
 //     Arjan Kok, Carel Bast                                                                                           ~
 //~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-val VERSION: String by project
-val GROUP: String by project
-
-group = GROUP
-version = VERSION
-
 defaultTasks("mvgCorrector", "test", "installDist", "mvgTagger")
+
+plugins {
+    id("org.modelingvalue.gradle.corrector") version "0.3.56"
+}
